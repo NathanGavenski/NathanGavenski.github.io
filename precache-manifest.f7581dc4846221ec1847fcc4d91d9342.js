58aeef3a9ff32cc9a2c42520ee63d8883c08c459
Nathan Gavenski <nathangavenski@gmail.com>
2020-08-14 15:21:01 -0300

    Updates

---
+++ precache-manifest.f7581dc4846221ec1847fcc4d91d9342.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "886c4f0a0c2f4114b5439a1a59db947d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc5f743907903a75ea2b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d23142843f594313e96e",
+    "url": "/static/css/main.a6c29fc4.chunk.css"
+  },
+  {
+    "revision": "dc5f743907903a75ea2b",
+    "url": "/static/js/2.f69cbeef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f69cbeef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23142843f594313e96e",
+    "url": "/static/js/main.38336527.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "ba6c663538df2798fa907f98426e88a8",
+    "url": "/static/media/avatar.ba6c6635.jpg"
+  }
+]);
