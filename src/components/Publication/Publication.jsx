50209e1f4c3ec21f75d9337491b15aabff04f3af
Nathan <nathangavenski@gmail.com>
2022-08-25 23:20:31 -0300

    Add BRACIS work and fix links spacing

--- src/components/Publication/Publication.jsx
+++ src/components/Publication/Publication.jsx
@@ -14,20 +14,47 @@ function template(title, authors, conference, year, bibtex, pdf, github, copy, c
         <span className="year">{' '}({year})</span>
         <br />
         <div className="links">
-          {pdf.length > 0 && <a href={pdf} target="_blank" rel="noreferrer" className="pdf">[PDF]</a>}
-          {bibtex.length > 0 &&
-            <div className="button">
-              <button className="bibtex" ref={copyRef} onClick={copy} onKeyDown={copy}>[Bibtex]</button>
-              <Overlay target={copyRef.current} show={copyState} placement="top">
-                {(props) => (
-                  <Tooltip id="overlay" {...props}>
-                    Copied!
-                  </Tooltip>
-                )}
-              </Overlay>
-            </div>
+          {
+            pdf.length > 0 
+            && <a href={pdf} target="_blank" rel="noreferrer" className="pdf">[PDF]</a>
           }
-          {github.length > 0 && <a href={github} target="_blank" rel="noreferrer" className="github">[GitHub]</a>}
+          {
+            bibtex.length > 0 
+            && (
+              <div className="button">
+                <button 
+                  className="bibtex" 
+                  ref={copyRef} 
+                  onClick={copy} 
+                  onKeyDown={copy} 
+                  style={{ marginLeft: (pdf.length > 0) ? "0.5rem" : "0"}
+                }>
+                  [Bibtex]
+                </button>
+                <Overlay target={copyRef.current} show={copyState} placement="top">
+                  {(props) => (
+                    <Tooltip id="overlay" {...props}>
+                      Copied!
+                    </Tooltip>
+                  )}
+                </Overlay>
+              </div>
+            )
+          }
+          {
+            github.length > 0 
+            && (
+              <a 
+                href={github} 
+                target="_blank" 
+                rel="noreferrer" 
+                className="github" 
+                style={{ marginLeft: (bibtex.length > 0) || (pdf.length > 0) ? "0.5rem" : "0"}}
+              >
+                [GitHub]
+              </a>
+            )
+          }
         </div>
       </div>
     </div>
